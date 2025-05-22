e89e80b34fc2d788e3c33d86ad383b4c37a328f4
littlepeace2005 <bryanwang.littlepeace@gmail.com>
2025-05-21 19:56:02 -0700

    FlipFlop.v

---
+++ src/FlipFlop.v
@@ -0,0 +1,13 @@
+module FlipFlop (
+    input wire CLK,
+    input wire Reset,
+    input wire [7:0] d,
+    output reg [7:0] q
+);
+    always @(posedge CLK or posedge Reset) begin
+        if (Reset)
+            q <= 8'h00;
+        else
+            q <= d;
+    end
+endmodule
