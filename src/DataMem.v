61a75b636a21642b27fc0c1406e3d3888f1999b3
Bryan Wang <bryanwang.littlepeace@gmail.com>
2025-05-21 22:52:39 -0700

    Update DataMem.v

--- src/DataMem.v
+++ src/DataMem.v
@@ -1,17 +1,17 @@
 module DataMem (
     input  wire        MemRead,
     input  wire        MemWrite,
-    input  wire [7:0]  add,
+    input  wire [8:0]  add,
     input  wire [31:0] write_data,
     output reg  [31:0] read_data
 );
-    reg [31:0] memory [0:63];
+    reg [31:0] memory [0:127];
     initial begin
         $readmemh("data.mem", memory);
     end
-    assign read_data = (MemRead) ? memory[add[7:2]] : 32'hxxxxxxxx;
+    assign read_data = (MemRead) ? memory[add[8:2]] : 32'hxxxxxxxx;
     always @(*) begin
         if (MemWrite)
-            memory[add[7:2]] = write_data;
+            memory[add[8:2]] = write_data;
     end
 endmodule
